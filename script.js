3bccc4aad827d4804a611a375de585182b28d3b9
AnnaKolesnichenko <117039891+AnnaKolesnichenko@users.noreply.github.com>
2023-06-24 16:31:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,12 @@
 
 
 
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
 
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+"Добавляем любимый фильм"
 
-5) Добавить нумерацию выведенных фильмов */
+5) Фильмы должны быть отсортированы по алфавиту */
 
 'use strict';
 
@@ -24,6 +25,10 @@ const advertise = document.querySelectorAll('.promo__adv img');
 const genre = document.querySelector('.promo__genre');
 const mainImg = document.querySelector('.promo__bg');
 const moviesList = document.querySelector('.promo__interactive-list');
+const addInput = document.querySelector('.adding__input');
+const form = document.querySelector('form.add');
+const checkbox = document.querySelector('[type="checkbox"]');
+const deleteBasket = document.querySelectorAll('.delete');
 
 
 advertise.forEach(item => {
@@ -35,9 +40,9 @@ genre.innerHTML = 'drama';
 mainImg.style.background = "url('/img/bg.jpg')";
 mainImg.style.backgroundSize = "cover";
 
-function movieList (movies) {
-    moviesList.innerHTML = '';
-    movieDB.movies.sort();
+function movieList (movies, container) {
+    container.innerHTML = '';
+    movies.sort();
 
     return movies.map((item, i) => {
         return `
@@ -47,8 +52,48 @@ function movieList (movies) {
             ` ;  
     }).join('');
 }
-const moviesInGallery = movieList(movieDB.movies);
+
+
+function addMovieToDB(e) {
+    e.preventDefault();
+    let inputMovie = addInput.value;
+    const checkFav = checkbox.checked;
+
+     if(inputMovie.trim() === '' || inputMovie.trim() === null) {
+        return;
+    }
+
+    if(inputMovie.length > 21) {
+        inputMovie.slice(0, 21);
+        inputMovie += '...';
+    }
+
+    if(checkFav) {
+        console.log('Add this movie to favorites');
+    }
+
+    movieDB.movies.push(inputMovie);
+    movieDB.movies.sort();
+    movieList(movieDB.movies, moviesList);
+    const moviesInGallery = movieList(movieDB.movies, moviesList);
+    moviesList.insertAdjacentHTML('beforeend', moviesInGallery);   
+    form.reset(); 
+}
+
+function deleteMovie(deleteBasket) {
+    deleteBasket.forEach((item, i) => {
+        item.addEventListener('click', () => {
+            item.parentElement.remove();
+            movieDB.movies.splice(i, 1);
+        });
+    });
+}
+
+
+
+const moviesInGallery = movieList(movieDB.movies, moviesList);
 moviesList.insertAdjacentHTML('beforeend', moviesInGallery);
+form.addEventListener('submit', addMovieToDB);
 
 
 
